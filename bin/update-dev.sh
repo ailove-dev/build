6dabc2472b2856d7430b895dc31c43aec3bb175c
root <root@vs04.southbridge.ru>
2011-01-06 16:47:34 +0300

    Add update-dev.sh script

---
+++ bin/update-dev.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+PROJECT=$1
+
+PATH=/usr/bin:/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin:$PATH
+LOCATION="$(cd -P -- "$(dirname -- "$0")" && pwd -P)/.."
+
+# read configuration
+if [ -f "$LOCATION/etc/project-dev.conf.dist" ]; then
+    . "$LOCATION/etc/project-dev.conf.dist"
+    if [ -f "$LOCATION/etc/project-dev.conf" ]; then
+	. "$LOCATION/etc/project-dev.conf"
+    fi
+else
+    echo "can't load $LOCATION/etc/project-dev.conf.dist, please fetch it from repository"
+    exit 0
+fi
+
+if [ -z "$PROJECT" ]; then
+    echo "use $0 <project>"
+    exit 0
+fi
+
+cd $WWW_PATH/$PROJECT/repo/master
+GIT_SSL_NO_VERIFY=true git pull
+
+GIT_SSL_NO_VERIFY=true git branch -a | grep "remotes" | grep -v "HEAD" | while read line
+do
+    BRANCH=`echo $line | awk --field-separator=/ '{print $3}'`
+
+    if [ "$BRANCH" = "master" ]; then
+	/srv/admin/bin/update-revision.sh git-test $BRANCH
+	exit
+    fi
+
+    if [ ! -d $WWW_PATH/$PROJECT/repo/$BRANCH ]; then
+	GIT_SSL_NO_VERIFY=true git clone -b $BRANCH $GIT_URL/$PROJECT $WWW_PATH/$PROJECT/repo/$BRANCH
+	exit
+    fi
+
+    cd $WWW_PATH/$PROJECT/repo/$BRANCH
+    GIT_SSL_NO_VERIFY=true git pull
+done
