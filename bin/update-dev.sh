712ec2b2157f9aeb0a5ac6c813b2147ac0d7849d
root <root@vs04.southbridge.ru>
2011-01-07 03:25:06 +0300

    algorithm improvments

--- bin/update-dev.sh
+++ bin/update-dev.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 PROJECT=$1
+BRANCH=$2
+ACTION=$3
 
 PATH=/usr/bin:/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin:$PATH
 LOCATION="$(cd -P -- "$(dirname -- "$0")" && pwd -P)/.."
@@ -25,23 +27,28 @@ if [ ! -d $WWW_PATH/$PROJECT/repo/master ]; then
     GIT_SSL_NO_VERIFY=true git clone $GIT_URL/$PROJECT $WWW_PATH/$PROJECT/repo/master
 fi
 
-cd $WWW_PATH/$PROJECT/repo/master
+#cd $WWW_PATH/$PROJECT/repo/master
+#GIT_SSL_NO_VERIFY=true git pull
+
+#GIT_SSL_NO_VERIFY=true git branch -a | grep "remotes" | grep -v "HEAD" | while read line
+#do
+#    BRANCH=`echo $line | awk --field-separator=/ '{print $3}'`
+#done
+
+if [ "$ACTION" = "delete" ]; then
+    rm -rf $WWW_PATH/$PROJECT/repo/$BRANCH
+    exit
+fi
+
+if [ ! -d $WWW_PATH/$PROJECT/repo/$BRANCH ]; then
+    GIT_SSL_NO_VERIFY=true git clone -b $BRANCH $GIT_URL/$PROJECT $WWW_PATH/$PROJECT/repo/$BRANCH
+    exit
+fi
+
+cd $WWW_PATH/$PROJECT/repo/$BRANCH
 GIT_SSL_NO_VERIFY=true git pull
 
-GIT_SSL_NO_VERIFY=true git branch -a | grep "remotes" | grep -v "HEAD" | while read line
-do
-    BRANCH=`echo $line | awk --field-separator=/ '{print $3}'`
-
-    if [ "$BRANCH" = "master" ]; then
-	/srv/admin/bin/update-revision.sh $PROJECT $BRANCH
-	exit
-    fi
-
-    if [ ! -d $WWW_PATH/$PROJECT/repo/$BRANCH ]; then
-	GIT_SSL_NO_VERIFY=true git clone -b $BRANCH $GIT_URL/$PROJECT $WWW_PATH/$PROJECT/repo/$BRANCH
-	exit
-    fi
-
-    cd $WWW_PATH/$PROJECT/repo/$BRANCH
-    GIT_SSL_NO_VERIFY=true git pull
-done
+if [ "$BRANCH" = "master" ]; then
+    /srv/admin/bin/update-revision.sh $PROJECT $BRANCH
+    exit
+fi
