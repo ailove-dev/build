4cb51b7156c89611e06e81024aa3f672fae96881
root <root@vs01.southbridge.ru>
2012-02-02 17:39:01 +0400

    cache update fix

---
+++ bin/update-cache.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+PROJECT=$1
+
+PATH=/usr/bin:/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin:$PATH
+LOCATION="$(cd -P -- "$(dirname -- "$0")" && pwd -P)/.."
+
+# read configuration
+if [ -f "$LOCATION/etc/project-dev.conf.dist" ]; then
+    . "$LOCATION/etc/project-dev.conf.dist"
+    if [ -f "$LOCATION/etc/project-dev.conf" ]; then
+	. "$LOCATION/etc/project-dev.conf"
+    fi
+else
+    echo "can't load $LOCATION/etc/project-dev.conf.dist, please fetch it from repository"
+    exit 0
+fi
+
+if [ -z "$PROJECT" ]; then
+    echo "use $0 <project>"
+    exit 0
+fi
+
+rm -rf $WWW_PATH/$PROJECT/cache/*
