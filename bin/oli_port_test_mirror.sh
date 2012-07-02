4e72a124f1eecb750582768846523b2e2fbcddc2
root <root@vs01.ailove.ru>
2012-07-02 18:54:18 +0400

    oli_port_test_mirror

---
+++ bin/oli_port_test_mirror.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+PROJECT="$1"
+
+PATH=/usr/bin:/bin:/sbin:/usr/sbin:/usr/local/bin:/usr/local/sbin:$PATH
+LOCATION="$(cd -P -- "$(dirname -- "$0")" && pwd -P)/.."
+
+# read configuration
+if [ -f "$LOCATION/etc/project-dev.conf.dist" ]; then
+    . "$LOCATION/etc/project-dev.conf.dist"
+    if [ -f "$LOCATION/etc/project-dev.conf" ]; then
+	. "$LOCATION/etc/project-dev.conf"
+    fi
+else
+    echo "can't load $LOCATION/etc/project-dev.conf.dist, please fetch it from repository"
+    exit 0
+fi
+
+$SUDO_PATH -u $SVN_USERNAME ssh -o StrictHostKeyChecking=no oli-port.mirror.ailove.ru "/srv/southbridge/script/mirror.sh"
