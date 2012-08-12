a06c2f89168d5678c73e79c5f71d22859164f50c
root <root@vs01.southbridge.ru>
2012-08-12 21:48:14 +0400

    cosmetic changes

--- bin/highload-report.sh
+++ bin/highload-report.sh
@@ -4,21 +4,26 @@ PATH="/sbin:/usr/sbin:/usr/local/sbin:/bin:/usr/bin:/usr/local/bin"
 
 STAMP=`date +%H%M%S`
 
-echo "<html><body><pre>" >> /tmp/$STAMP.tmp
+echo "<html><body>" >> /tmp/$STAMP.tmp
 echo "<h3>load average</h3>" >> /tmp/$STAMP.tmp
+echo "<p><pre>" >> /tmp/$STAMP.tmp
 echo >> /tmp/$STAMP.tmp
 uptime >> /tmp/$STAMP.tmp 2>&1
 echo >> /tmp/$STAMP.tmp
+echo "</pre></p>" >> /tmp/$STAMP.tmp
 
 if [ -f "/root/.mysql" ]; then
     echo "<h3>mysql processes</h3>" >> /tmp/$STAMP.tmp
+    echo "<p><pre>" >> /tmp/$STAMP.tmp
     echo >> /tmp/$STAMP.tmp
     mysql -u root -p`cat /root/.mysql` -e "SHOW FULL PROCESSLIST" | sort -n -k 6 >> /tmp/$STAMP.tmp 2>&1
     echo >> /tmp/$STAMP.tmp
+    echo "</pre></p>" >> /tmp/$STAMP.tmp
 fi
 
 if [ -f "/root/.postgresql" ]; then
     echo "<h3>postgresql processes</h3>" >> /tmp/$STAMP.tmp
+    echo "<p><pre>" >> /tmp/$STAMP.tmp
     echo >> /tmp/$STAMP.tmp
 
     if [ -f "/etc/init.d/pgbouncer" ]; then
@@ -29,60 +34,79 @@ if [ -f "/root/.postgresql" ]; then
 
     echo "SELECT datname,procpid,current_query FROM pg_stat_activity;" | psql -U postgres --port=$PORT >> /tmp/$STAMP.tmp 2>&1
     echo >> /tmp/$STAMP.tmp
+    echo "</pre></p>" >> /tmp/$STAMP.tmp
 fi
 
 echo "<h3>memory process list (top100)</h3>" >> /tmp/$STAMP.tmp
+echo "<p><pre>" >> /tmp/$STAMP.tmp
 echo >> /tmp/$STAMP.tmp
 ps -ewwwo size,command --sort -size | head -100 | awk '{ hr=$1/1024 ; printf("%13.2f Mb ",hr) } { for ( x=2 ; x<=NF ; x++ ) { printf("%s ",$x) } print "" }' >> /tmp/$STAMP.tmp 2>&1
 echo >> /tmp/$STAMP.tmp
+echo "</pre></p>" >> /tmp/$STAMP.tmp
 
 echo "<h3>process list (sort by cpu)</h3>" >> /tmp/$STAMP.tmp
+echo "<p><pre>" >> /tmp/$STAMP.tmp
 echo >> /tmp/$STAMP.tmp
 ps -ewwwo pcpu,pid,user,command --sort -pcpu >> /tmp/$STAMP.tmp 2>&1
 echo >> /tmp/$STAMP.tmp
+echo "</pre></p>" >> /tmp/$STAMP.tmp
 
 LINKSVER=`links -version | grep "2.2" | wc -l`
 if [ $LINKSVER -gt 0 ]; then
     echo "<h3>apache</h3>" >> /tmp/$STAMP.tmp
+    echo "<p><pre>" >> /tmp/$STAMP.tmp
     echo >> /tmp/$STAMP.tmp
     links -dump -retries 1 -receive-timeout 30 http://localhost:8080/apache-status >> /tmp/$STAMP.tmp 2>&1
     echo >> /tmp/$STAMP.tmp
+    echo "</pre></p>" >> /tmp/$STAMP.tmp
 
     echo "<h3>nginx</h3>" >> /tmp/$STAMP.tmp
+    echo "<p><pre>" >> /tmp/$STAMP.tmp
     echo >> /tmp/$STAMP.tmp
     links -dump -retries 1 -receive-timeout 30 http://localhost/nginx-status >> /tmp/$STAMP.tmp 2>&1
     echo >> /tmp/$STAMP.tmp
+    echo "<p><pre>" >> /tmp/$STAMP.tmp
 else
     echo "<h3>apache</h3>" >> /tmp/$STAMP.tmp
+    echo "<p><pre>" >> /tmp/$STAMP.tmp
     echo >> /tmp/$STAMP.tmp
     links -dump -eval 'set connection.retries = 1' -eval 'set connection.receive_timeout = 30' http://localhost:8080/apache-status >> /tmp/$STAMP.tmp 2>&1
     echo >> /tmp/$STAMP.tmp
+    echo "</pre></p>" >> /tmp/$STAMP.tmp
 
     echo "<h3>nginx</h3>" >> /tmp/$STAMP.tmp
+    echo "<p><pre>" >> /tmp/$STAMP.tmp
     echo >> /tmp/$STAMP.tmp
     links -dump -eval 'set connection.retries = 1' -eval 'set connection.receive_timeout = 30' http://localhost/nginx-status >> /tmp/$STAMP.tmp 2>&1
     echo >> /tmp/$STAMP.tmp
+    echo "</pre></p>" >> /tmp/$STAMP.tmp
 fi
 
 echo "<h3>connections report</h3>" >> /tmp/$STAMP.tmp
+echo "<p><pre>" >> /tmp/$STAMP.tmp
 echo >> /tmp/$STAMP.tmp
 netstat -plan | grep :80 | awk {'print $5'} | cut -d: -f 1 | sort | uniq -c | sort -n >> /tmp/$STAMP.tmp 2>&1
 echo >> /tmp/$STAMP.tmp
+echo "</pre></p>" >> /tmp/$STAMP.tmp
 
 echo "<h3>syn tcp/udp session</h3>" >> /tmp/$STAMP.tmp
+echo "<p><pre>" >> /tmp/$STAMP.tmp
 echo >> /tmp/$STAMP.tmp
 netstat -n | egrep '(tcp|udp)' | grep SYN | wc -l >> /tmp/$STAMP.tmp 2>&1
 echo >> /tmp/$STAMP.tmp
+echo "</pre></p>" >> /tmp/$STAMP.tmp
 
 if [ -f "/root/.mysql" ]; then
     echo "<h3>mysql status</h3>" >> /tmp/$STAMP.tmp
+    echo "<p><pre>" >> /tmp/$STAMP.tmp
     echo >> /tmp/$STAMP.tmp
     mysql -u root -p`cat /root/.mysql` -e "SHOW STATUS where value !=0" >> /tmp/$STAMP.tmp 2>&1
     echo >> /tmp/$STAMP.tmp
+    echo "</pre></p>" >> /tmp/$STAMP.tmp
 fi
 
 SUBJECT="`hostname` load"
-echo "</pre></body></html>" >> /tmp/$STAMP.tmp
+echo "</body></html>" >> /tmp/$STAMP.tmp
 
 cat - /tmp/$STAMP.tmp <<EOF | sendmail -oi -t
 To: root
